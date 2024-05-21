eb096ba0563937755458308bcc40eeaae13b08e0
Bartosz <gazdrinker2@gmail.com>
2024-05-21 21:21:01 +0200

    add portfolio section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,35 +7,35 @@ import { HiMiniWrenchScrewdriver } from 'react-icons/hi2';
 import { SlSpeedometer } from 'react-icons/sl';
 
 export default function Services() {
+	const services = [
+		{
+			icon: FaCode,
+			title: 'Web Design',
+			text: 'Visually appealing, user-friendly websites tailored to business needs and goals. Service includes layout, color scheme, typography and imagery design.',
+		},
+		{
+			icon: HiMiniWrenchScrewdriver,
+			title: 'Web Maintenance',
+			text: 'Continuous support and maintenance for website updates, content management and troubleshooting.',
+		},
+		{
+			icon: SlSpeedometer,
+			title: 'Web Optimization',
+			text: 'Website speed, performance, and search engine ranking optimization through image compression, code minification, caching and SEO.',
+		},
+	];
+
 	return (
 		<section className='wrapper services section-container'>
 			<h2 className='services__title section-title'>My Services</h2>
 			<div className='services__boxes'>
-				<div className='services__boxes-box'>
-					<FaCode />
-					<h3 className='services__boxes-box-title'>Web Design</h3>
-					<p className='services__boxes-box-text'>
-						Visually appealing, user-friendly websites tailored to business
-						needs and goals. Service includes layout, color scheme, typography
-						and imagery design.
-					</p>
-				</div>
-				<div className='services__boxes-box'>
-					<HiMiniWrenchScrewdriver />
-					<h3 className='services__boxes-box-title'>Web Maintenance</h3>
-					<p className='services__boxes-box-text'>
-						Continuous support and maintenance for website updates, content
-						management and troubleshooting.
-					</p>
-				</div>
-				<div className='services__boxes-box'>
-					<SlSpeedometer />
-					<h3 className='services__boxes-box-title'>Web Optimization</h3>
-					<p className='services__boxes-box-text'>
-						Website speed, performance, and search engine ranking optimization
-						through image compression, code minification, caching and SEO.
-					</p>
-				</div>
+				{services.map((service, index) => (
+					<div key={index} className='services__boxes-box'>
+						<service.icon />
+						<h3 className='services__boxes-box-title'>{service.title}</h3>
+						<p className='services__boxes-box-text'>{service.text}</p>
+					</div>
+				))}
 			</div>
 		</section>
 	);
--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -2,6 +2,7 @@ import Navbar from '../components/navbar/Navbar';
 import Header from '../components/header/Header';
 import About from '../components/about/About';
 import Services from '../components/services/Services';
+import Portfolio from '../components/portfolio/Portfolio'
 
 export default function PageLayout() {
 	return (
@@ -11,6 +12,7 @@ export default function PageLayout() {
 			<main>
 				<About />
 				<Services />
+				<Portfolio />
 				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 			</main>
 		</>
