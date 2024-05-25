659d3430d8d59ce2c5262c566c0f7f4a492b5b53
Bartosz <gazdrinker2@gmail.com>
2024-05-25 19:00:53 +0200

    add: services subtitle

--- src/i18n.js
+++ src/i18n.js
@@ -1,36 +1,34 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import Backend from "i18next-http-backend"
+import Backend from 'i18next-http-backend';
 
-import langPL from '../public/locales/pl/translation.json'
-import langEN from '../public/locales/en/translation.json'
+import langPL from '../public/locales/pl/translation.json';
+import langEN from '../public/locales/en/translation.json';
 
 i18n
 	.use(LanguageDetector)
 	.use(initReactI18next)
-    .use(Backend)
+	.use(Backend)
 	.init({
 		debug: true,
 		lng: 'pl',
-        fallbackLng: 'pl',
-        returnObjects: true,
-		resources: {
-			pl: {
-			  common: langPL,
-			},
-			en: {
-			  common: langEN,
-			},
-		  },
-		  ns: ["common"],
-
+		fallbackLng: 'pl',
+		returnObjects: true,
+		// resources: {
+		// 	pl: {
+		// 		common: langPL,
+		// 	},
+		// 	en: {
+		// 		common: langEN,
+		// 	},
+		// },
+		ns: ['common'],
 
 		interpolation: {
-			escapeValue: false
+			escapeValue: false,
 		},
-		// backend: {
-		// 	loadPath: '/my-portfolio/locales/{{lng}}/translation.json'
-		// }
-
+		backend: {
+			loadPath: '/my-portfolio/locales/{{lng}}/translation.json'
+		}
 	});
